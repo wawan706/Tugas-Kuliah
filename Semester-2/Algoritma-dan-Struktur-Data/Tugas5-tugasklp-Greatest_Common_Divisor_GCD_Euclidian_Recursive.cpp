5d6b2bfdf443b0357dda61ae52c70fb01ee1bc91
Wawan <138946507+wawan706@users.noreply.github.com>
2026-03-30 15:30:38 +0800

    Implement GCD using Euclidean algorithm recursively

--- Semester-2/Algoritma-dan-Struktur-Data/Tugas5-tugasklp-Greatest_Common_Divisor_GCD_Euclidian_Recursive.cpp
+++ Semester-2/Algoritma-dan-Struktur-Data/Tugas5-tugasklp-Greatest_Common_Divisor_GCD_Euclidian_Recursive.cpp
@@ -1,1 +1,27 @@
+// Kelompok 5 Teknik Informatika Kelas B
 
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    
+    return gcd(b, a % b);
+}
+
+int main() {
+    int a = 48;
+    int b = 18;
+
+    cout << "--- Program Pencari GCD (Rekursif) ---" << endl;
+    cout << "Mencari nilai GCD(" << a << ", " << b << ")" << endl;
+    
+    int hasil = gcd(a, b);
+    
+    cout << "Hasil akhir GCD   = " << hasil << endl;
+    cout << "--------------------------------------" << endl;
+
+    return 0;
+}
